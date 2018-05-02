d5bd5e91210b2c0dbf0bb7b4f1c45a80af0f3df2
Andrew Jones <andrewghjones@gmail.com>
2018-05-02 16:19:19 +0100

    Removed old comments. Still to address the year, month and proximity to death issues.

--- sql/patient_level_1415.sql
+++ sql/patient_level_1415.sql
@@ -1,3 +1,5 @@
+/****** PROXIMITY TO DEATH BY MONTH PROVISIONAL SCRIPT  ******/
+
 
 -- It may be sensible, if looking especially at the trends in last 12 months of life,
 -- to exclude activity for a year before latest recorded deaths.
@@ -14,28 +16,26 @@ SELECT *
 				)
 
 	ELSE cte.prox_to_death
-  END [lower_bound]-- LOWER BOUND GOES HERE : ADMISSION AND END OF DEATHS DATA
+  END [lower_bound] -- DiFFERENCE BETWEEN ADMISSION AND END OF DEATHS DATA
 
 FROM (
   
   SELECT TOP 100000
-    -- Datepart(YEAR, ip.disdate) as [year]
+  
   CASE 
 	  WHEN disdate = '1801-01-01'
-	  THEN Datepart(YEAR, ip.epiend)
+	  THEN ISNULL(Datepart(YEAR, ip.epiend), DATEPART(YEAR, ip.epistart))
+	  -- THIS NEEDS TO BE FIXED, MAY BE epiend which is the problem.
 	  ELSE ISNULL(Datepart(YEAR, ip.disdate), Datepart(YEAR, ip.epiend)) 
 	  END [year]
 	
-   -- CASE 
-   --   WHEN Datepart(YEAR, ip.disdate) as [year] IS NOT NULL
-  	--THEN Datepart(YEAR, ip.disdate) as [year]
-    --, Datepart(YEAR, ip.admidate) as [year_admi]
+ 
   ,CASE 
 	WHEN disdate = '1801-01-01'
-	THEN Datepart(MONTH, ip.epiend)
+	THEN ISNULL(Datepart(MONTH, ip.epiend), Datepart(MONTH, ip.epistart)) 
 	ELSE ISNULL(Datepart(MONTH,ip.disdate), Datepart(MONTH, ip.epiend))
 	END [month]
-    --, Datepart(MONTH,ip.disdate) as [month] 
+  
   ,ip.encrypted_hesid
    
     ,CASE 
@@ -53,13 +53,9 @@ FROM (
                              ,CONVERT(DATE, CONVERT(NVARCHAR(4), DATepart(YY, ip.epiend))+ '-01-01')
                               ) / 365)
 					 ) 
-					 END [age_jan1]
+	   END [age_jan1]
 	-- from hesdata.dbo.tbinpatients1516 ip
-    --,DATEDIFF(Y
-    --          , CONVERT(DATE, SUBSTRING(ip.mydob, 3, 4) + '-' + SUBSTRING(ip.mydob, 1, 2) + '-16')
-    --          , CONVERT(DATE, CONVERT(NVARCHAR(4), DATepart(YY, ip.disdate))+ '-01-01')
-    --          --, CONVERT(DATE, '2015' + '-01-01') -- FIX: episodes could fall in one of two years. 
-    --          ) / 365 as [age_jan1]
+
     --,ip.startage
 	,ip.sex as [gender]    
     ,ip.admidate
@@ -68,16 +64,19 @@ FROM (
    
     --, resladst_ons
     , lsoa01
-    --, lsoa11
+    --, lsoa11 -- for years after 2015
    
     , ISNULL(DATEDIFF(dd, ip.admidate, ip.disdate) 
 	         ,DATEDIFF(dd, ip.admidate, ip.epiend)
-			 ) as [beddays] -- is there a better way to count bed days
-    ,cause_of_death
-    ,LEFT(cause_of_death, 1) as [cod_1]
+			 ) as [beddays] -- is this a naive way to count bed days?
+    
+	,cause_of_death
+    
+	,LEFT(cause_of_death, 1) as [cod_1]
+   
     , CASE
         WHEN d.Encrypted_HESid IS NULL
-        THEN NULL -- SET NULLS FOR THOSE WITHOUT DEATH RECORD 
+        THEN NULL -- SET AS NULL FOR THOSE WITHOUT DEATH RECORD 
         WHEN DATEDIFF(dd, ip.admidate, d.DOD) between 0 and 40000 
         THEN CAST(
           FLOOR(
@@ -93,23 +92,20 @@ FROM (
     
   FROM [HESData].dbo.tbInpatients1415 ip
   
-    -- LEFT OUTER JOIN [StrategicReference].dbo.vwGPPracticeToCCGAndPCT ccg 
-    --   ON ip.gpprac = ccg.GPPractice 
-    
     LEFT OUTER JOIN 
     (
-    SELECT
-     a.[Encrypted_HESID]
-    ,a.[DOD]
-    ,a.[CAUSE_OF_DEATH]
-    --,a.derivedage
-    ,a.SUBSEQUENT_ACTIVITY_FLG
+      SELECT
+       a.[Encrypted_HESID]
+      ,a.[DOD]
+      ,a.[CAUSE_OF_DEATH]
+     
+      ,a.SUBSEQUENT_ACTIVITY_FLG
+      
+      FROM [ONS].[HESONS].[tbMortalityto1516] a
     
-    FROM [ONS].[HESONS].[tbMortalityto1516] a
-    --WHERE DOD between '20", str_sub(years[i], 1, 2), "-04-01' AND '20", year_plus_one , "-03-31'
-    WHERE DOD between '2014-04-01' AND '2016-03-31'
-    AND SUBSEQUENT_ACTIVITY_FLG IS NULL -- Ignore deaths with subseq activity.
-    ) d
+      WHERE DOD between '2014-04-01' AND '2016-03-31'
+      AND SUBSEQUENT_ACTIVITY_FLG IS NULL -- Ignore deaths with subseq activity.
+     ) d
     ON ip.Encrypted_HESID = d.Encrypted_HESID
     
     ---
