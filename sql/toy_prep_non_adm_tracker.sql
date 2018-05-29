5d07130f71087d4c740a7e2fcf4965932cb7f63b
Andrew Jones <andrewghjones@gmail.com>
2018-05-29 10:32:27 -0400

    Toy tracker complete.

--- sql/toy_prep_non_adm_tracker.sql
+++ sql/toy_prep_non_adm_tracker.sql
@@ -1,28 +1,19 @@
---SELECT*
---FROM 
---  (VALUES
---     ('A', 2007)
---    ,('A', 2009)
---    ,('B', 2008)
---     ) as teens2 (id, yr)
 
---RIGHT JOIN (
-
-SELECT *
+SELECT DISTINCT [id], [year2] --, [yr]
 FROM (
       SELECT *
       
       FROM 
-	  --- TABLE 1:
+	  --- ORIGINAL TABLE 1:
         (VALUES
            ('A', 2007)
           ,('A', 2009)
           ,('B', 2008)
-           ) as teens(id, yr)
+           ) as orig(id, yr)
       
       
       LEFT JOIN (
-	  -- TO TABLE 2, CREATING TABLE 3:
+	  -- TO TABLE 2 (years on the fly), CREATING TABLE 3 (for every year w/ episode - give years w/o):
 
       			SELECT  [year]
       				   ,[year2]
@@ -31,102 +22,24 @@ FROM (
       			  (VALUES (2007),(2008),(2009),(2010)) as g ([year])
       			  cross join (VALUES (2007),(2008),(2009),(2010)) a([year2])
       			  WHERE [year] <> [year2]
-      			  ) y
+      			  ) table2
       			  
-        	ON teens.yr = y.year
-	) T3
-	WHERE NOT EXISTS (SELECT 1
-						FROM (
-								SELECT *
-								FROM 
-								--- TABLE 1:
-								  (VALUES
-								     ('A', 2007)
-								    ,('A', 2009)
-								    ,('B', 2008)
-								     ) AS teens2(id, yr)
-								) T1
+        	    ON orig.yr = table2.year
+	) CTE1
+	
+    WHERE NOT EXISTS (
+                      SELECT 1
+					  FROM (
+							SELECT *
+							FROM (
+							-- TABLE 1:
+							    VALUES
+							      ('A', 2007)
+							      ,('A', 2009)
+							      ,('B', 2008)
+							      ) AS teens2(id, yr)
+							) orig1
 								
-								WHERE T3.year2 = T1.yr
+							WHERE (CTE1.year2 = orig1.yr AND CTE1.id = orig1.id)
 						)
 
-     
-/***** 
-OTHER WAY ROUND
-DOES NOT WORK
-*******/
-
-
-SELECT *
-FROM (
-	VALUES ('A', 2007)
-          ,('A', 2009)
-		  ,('B', 2008)
-	) AS teens2(id, yr)
- 
-	WHERE not EXISTS (
-						SELECT 1
-						FROM (
-								SELECT *
-								FROM 
-								--- TABLE 1:
-								  (
-								   SELECT *
-      
-									FROM 
-									--- TABLE 1:
-									  (VALUES
-									     ('A', 2007)
-									    ,('A', 2009)
-									    ,('B', 2008)
-									     ) as teens(id, yr)
-									
-									
-									LEFT JOIN (
-									-- TO TABLE 2, CREATING TABLE 3:
-
-												SELECT  [year]
-													   ,[year2]
-									
-												FROM
-												  (VALUES (2007),(2008),(2009),(2010)) as g ([year])
-												  cross join (VALUES (2007),(2008),(2009),(2010)) a([year2])
-												  WHERE [year] <> [year2]
-												  ) y
-												  
-									  	ON teens.yr = y.year
-	
-   
-																) a) T1
-													WHERE teens2.yr = T1.year2			
-																
-														)
-									 
-									 
-	 
-   
-   
-
-   
-   
-   
-   
-   
-        
-	---- ANTI JOIN
-	--WHERE NOT EXISTS (
-	--				  SELECT 1 FROM ( 
-					  
-	--				  --- JOIN TWO TABLES TO CREATE INDEX OF DUPLICATES
-
-	--								 SELECT new.encrypted_hesid
-	--								 FROM [ONS].[HESONS].[tbMortality1516] new
-	--									INNER JOIN [ONS].[HESONS].[tbMortalityto1415] old
-	--									ON new.encrypted_hesid = old.encrypted_hesid AND old.[File] = '1314to1415'
-	
-	--									WHERE DATEPART(MONTH, OLD.DOD)  > 03
-	--									AND DATEPART(YEAR, oLD.DOD)  >= 2015 
-	--									) duplicates
-										
-	--					WHERE duplicates.encrypted_hesid = mort.encrypted_hesid
-	--					)
